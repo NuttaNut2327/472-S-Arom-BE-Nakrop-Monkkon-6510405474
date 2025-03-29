882db63049a9f1649a87ae0a16408f650d977107
Nutta <117891147+NuttaNut2327@users.noreply.github.com>
2025-03-29 15:02:18 +0700

    Add: Feature/send email notify task (#1) (#2)
    - Add feature send email when Create task
    - Add feature send email when Update task
    - Add function to set time to send email to remind task.
    - Add Unit test & GitHub Actions

--- configs/config.go
+++ configs/config.go
@@ -12,12 +12,16 @@ type Config struct {
 	DB_USER  		string `mapstructure:"DB_USER"`
 	DB_PASSWORD 	string `mapstructure:"DB_PASSWORD"`
 	DB_PORT 		string `mapstructure:"DB_PORT"`
-	JWT_SECRET	string `mapstructure:"JWT_SECRET"`
-	SUPABASE_URL     string `mapstructure:"SUPABASE_URL"`
+	JWT_SECRET		string `mapstructure:"JWT_SECRET"`
+	SUPABASE_URL    string `mapstructure:"SUPABASE_URL"`
 	SUPABASE_API_KEY string `mapstructure:"SUPABASE_API_KEY"`
-	SUPABASE_BUCKET  string `mapstructure:"SUPABASE_BUCKET"`
-	// ADMIN_EMAIL 	string `mapstructure:"ADMIN_EMAIL"`
-	// ADMIN_PASSWORD 	string `mapstructure:"ADMIN_PASSWORD"`
+	SUPABASE_BUCKET string `mapstructure:"SUPABASE_BUCKET"`
+	
+	// SMTP settings
+	SMTP_HOST       string `mapstructure:"SMTP_HOST"`
+	SMTP_PORT       int    `mapstructure:"SMTP_PORT"`
+	EMAIL_FROM      string `mapstructure:"SMTP_USER"`
+	EMAIL_PASSWORD  string `mapstructure:"SMTP_PASSWORD"`
 }
 
 func NewConfig() *Config {
@@ -30,6 +34,11 @@ func NewConfig() *Config {
 
 	if err := viper.Unmarshal(config); err != nil {
 		log.Fatalln("❌ Unable to decode into struct", err)
+	}
+
+	// Add default values for SMTP port if not set in env
+	if config.SMTP_PORT == 0 {
+		config.SMTP_PORT = 587 // Default SMTP port
 	}
 
 	return config
